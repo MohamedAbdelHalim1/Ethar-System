5709a36b7b387aae99f4ce1e0166df31d04f1ab9
Mohamed Abdelhalim <mohamedabdelhalim2804@gmail.com>
2025-06-25 17:55:09 +0300

    location reports7

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -34,6 +34,15 @@
         </div>
     </div>
     @push('scripts')
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+        <!-- DataTables CSS -->
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+
+        <!-- DataTables JS -->
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
         <script>
             $(document).ready(function() {
                 const table3 = document.getElementById('locations-table');
